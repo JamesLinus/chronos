b760f9f2d17ccbe5a2673313ae5b5fdcb6969ea6
Brenden Matthews <brenden@diddyinc.com>
2017-01-27 11:09:37 -0800

    Workaround for cert path bug.

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-export LIBPROCESS_PORT="${PORT1}" 
+if [ -d "$MESOS_SANDBOX/.ssh" ]; then
+  cp -r $MESOS_SANDBOX/.ssh ./
+fi
+
+export LIBPROCESS_PORT="${PORT1}"
 exec java $JVM_OPTS -jar /chronos/chronos.jar $@ --http_port $PORT0
